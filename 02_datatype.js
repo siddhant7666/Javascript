e8a8539498b8c4770b4f719db4f1ab00fd12547f
Siddhant <dearsidd2017@gmail.com>
2024-07-25 16:27:49 +0530

    done with datatype in js

---
+++ 02_datatype.js
@@ -0,0 +1,23 @@
+"use strict";
+
+//datatype
+console.log("Siddhant")
+
+let name = "Siddhant" //string
+let age = 18 //number
+let isLoggedIn = false; //boolean
+
+
+// null => standalone value {can be assigned}
+
+// undefined {no value defined or assigned}
+
+//symbol => unique
+
+//object => will do it later
+
+
+console.log(typeof age); //number
+console.log(typeof null); //object
+console.log(typeof undefined); //undefined
+console.log(typeof "Siddhant") //string
