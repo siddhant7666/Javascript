a1b155d5b4b8b049279a298a497b2fe8f0246a51
Siddhant <dearsidd2017@gmail.com>
2024-07-25 16:16:02 +0530

    learnt the concept of let, var, const, table printing

--- test.js
+++ test.js
@@ -1,1 +1,20 @@
 console.log("Siddhant")
+
+const accountId = 144553
+let accountEmail = "abcd@gmail.com"
+var accountPassword = "12345"
+// Don't user VAR in JS 
+
+accountCity = "Delhi"
+
+let accountState // undefined
+
+// accountId = 2
+
+// console.log(accountId)
+
+accountEmail="hello@gmail.com"
+accountPassword="lululu"
+accountCity="Gurgaon"
+
+console.table([accountId,accountEmail,accountPassword,accountCity, accountState])
