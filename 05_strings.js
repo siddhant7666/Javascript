cf92d5c22a6ee9e5378ba04e5f5a2a8fa0c3c838
Siddhant <dearsidd2017@gmail.com>
2024-07-29 16:19:31 +0530

    strings and its various operations and methods

--- 05_strings.js
+++ 05_strings.js
@@ -1,3 +1,23 @@
 let userName =  "Siddhant"
+const age = 22;
 console.log(userName);
-console.log("Hello Ji, " + userName);
+// console.log("Hello Ji, " + userName);
+// don't write like these  in modern day js
+
+
+// use backtick(``)
+// String interpolation is a computer programming technique that evaluates a string literal with placeholders and replaces them with their corresponding values
+
+
+console.log(`Hello, my userName is ${userName} and my age is ${age}`);
+
+
+
+const gameName = new String('HelloWorld')
+
+console.log(gameName[0]); // H
+
+const newgameName = " Namaste Duniya"
+console.log(newgameName[0]); // space will get printed
+console.log(newgameName[1]); // a
+
