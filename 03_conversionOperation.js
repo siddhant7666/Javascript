9a4b84f29e52062c4ec6b03bc84e0abbcf47362f
Siddhant <dearsidd2017@gmail.com>
2024-07-29 14:50:15 +0530

    Conversion of datatype - done

---
+++ 03_conversionOperation.js
@@ -0,0 +1,15 @@
+let score = "33abc"
+
+console.log(typeof(score));
+
+let valueInNumber = Number(score) // CONVERSION OF STRING TO NUMBER 
+
+console.log(valueInNumber);
+console.log(typeof valueInNumber);
+
+
+// "33" => 33
+// "33abc"  => NaN  
+// "true" => 1, "false" => 0
+
+
