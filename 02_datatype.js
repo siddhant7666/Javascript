82a146755e8efdf00a89a5fce0c0a0136b213cc1
Siddhant <dearsidd2017@gmail.com>
2024-07-29 15:39:22 +0530

    types of datatypes in js

--- 02_datatype.js
+++ 02_datatype.js
@@ -28,3 +28,31 @@ console.log(2%2)
 console.log(2-2)
 console.log(3*2)
 console.log(5%2)
+
+
+// Two types of datatype
+
+//1. PRIMITIVE 
+
+//7 TYPES (CALL BY VALUE): STRING, NUMBER, BOOLEAN, NULL, UNDEFINED, SYMBOL, BigInt
+
+//2. REFERENCE TYPE (Non - Primitive) - Call by reference
+
+// Array, Objects, Functions
+
+
+ //Is JS statically defined or dynamically defined?
+
+/*
+
+Answer: 
+
+JavaScript is a dynamically typed language. This means that the type of a variable is determined at runtime rather than at compile-time. In JavaScript, you don't need to explicitly specify the data type of a variable when you declare it; the type is inferred based on the value assigned to it. For example:
+
+
+let x = 5;        // x is initially a number
+x = "Hello";      // x is now a string
+In this example, the variable x changes its type from a number to a string, demonstrating the dynamic nature of JavaScript.
+
+
+*/
