6a8aa6568c35e174fe87651f52d0ea9ea9d4b9e8
Siddhant <dearsidd2017@gmail.com>
2024-07-29 15:02:32 +0530

    various operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -13,3 +13,43 @@ console.log(typeof valueInNumber);
 // "true" => 1, "false" => 0
 
 
+/**********OPERATIONS************/
+
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+
+
+
+let str1 = "Siddhant"
+let str2 = " Singh"
+let str3 = str1 + str2
+
+console.log(str3);
+
+
+console.log("1" + 2); //12
+console.log(1+"2"); //12
+console.log("1" + 2 + 2); //122
+console.log(1 + 2 + "2"); // 32
+
+
+console.log(true);  // TRUE
+console.log(+true);  // 1
+// console.log(true+);  // ERROR
+
+console.log(+""); //0
+
+// let num1, num2, num3
+
+
+// num1 = num2 = num3 = 2 + 2
+
+// not a good way for coding as less readable
+
+
+let gameCounter = 100
+gameCounter++;
+
+console.log(gameCounter); // 101
