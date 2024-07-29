87b9cd37fd96fb2fa0bb2fbb7707b46d66640485
Siddhant <dearsidd2017@gmail.com>
2024-07-29 15:10:30 +0530

    comparison in js

---
+++ 04_comparison.js
@@ -0,0 +1,17 @@
+console.log(3>1);
+console.log(1>3);
+console.log(1==3);
+console.log(1!=1);
+
+
+console.log("2" > 1);   //true
+console.log("02" > 1);  //true // make sure datatype is same while comparison
+
+console.log(null > 0);   //false
+console.log(null == 0);  //false
+console.log(null >= 0);  //true -> null got converted in 0 here
+
+
+console.log(undefined == 0); //false
+console.log(undefined == 0); //false
+console.log(undefined == 0); //false
